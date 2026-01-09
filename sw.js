bf4a5238f49f7a7d1a742ab72352528134a7f649
Adam Radvan <adamradvan@gmail.com>
2026-01-09 22:11:07 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'histali-v1';
+const ASSETS_TO_CACHE = [
+  '/histali/',
+  '/histali/index.html',
+  '/histali/data/sk.json',
+  '/histali/icons/icon-192.png',
+  '/histali/icons/icon-512.png',
+  '/histali/icons/apple-touch-icon.png',
+  '/histali/manifest.json'
+];
+
+// Install event - cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event - clean old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((response) => {
+        // Don't cache non-successful responses or non-GET requests
+        if (!response || response.status !== 200 || event.request.method !== 'GET') {
+          return response;
+        }
+        // Clone and cache the response
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
